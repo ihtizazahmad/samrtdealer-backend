61b42988761eb874f21824157add07080549ec41
Mustafa zeb <tauheedzeb381@gmail.com.com>
2022-04-28 15:46:14 +0500

    api connect to online mongodb cluster

--- config/config.js
+++ config/config.js
@@ -1,5 +1,20 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/patronwork').then(() => {
-    console.log('Connected to MongoDB');
+// const mongoose = require('mongoose');
+// mongoose.connect('mongodb://localhost:27017/patronwork').then(() => {
+//     console.log('Connected to MongoDB');
 
-}).catch(err => { console.log(err) });  
+// }).catch(err => { console.log(err) });  
+
+// const { ServerApiVersion } = require('mongodb')
+const mongoose = require('mongoose')
+const URLCONFIG = "mongodb+srv://mustafakhan:MirtBtNr08MG2Mlt@cluster0.wjhbs.mongodb.net/patronwork?retryWrites=true&w=majority"
+mongoose.connect(URLCONFIG).then(() => {
+    // ServerApiVersion = true
+
+    console.log("connected to db")
+}).catch(err => { console.log(err) })
+
+// const mongoose = require('mongoose');
+
+// mongoose.connect('mongodb://locahost:27017/e-comm',).then(() => {
+//     console.log("connected to db")
+// }).catch(err=>{console.log(err)})
