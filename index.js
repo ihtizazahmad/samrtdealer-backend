2fa04346db117fb70bb8cff132215057b202c76a
Saadkhancode <saadk6401@gmail.com>
2022-05-13 16:30:50 +0500

    Added a Procfile.

--- index.js
+++ index.js
@@ -17,19 +17,18 @@ app.use(express.json());
 app.use('/', payerrouter)
 app.use('/', tablerouter)
 
-app.use(cors());
-
-app.use(cors({
-    origin: 'http://localhost:18010',
-    method: ['GET', 'POST', 'PUT', 'DELETE'],
-}))
+const corsOptions ={
+    origin:'http://localhost:18010', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 
 app.use((req, res, next) => {
     
     res.append('Access-Control-Allow-Headers', 'Content-Type');
     res.setHeader("Access-Control-Allow-Origin", "http://localhost:18010")
     res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Max-Age", "1800");
     res.setHeader("Access-Control-Allow-Headers", "content-type");
     res.setHeader("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS");
     next();
