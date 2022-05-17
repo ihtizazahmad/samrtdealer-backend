09628c43469d68721b2623c226dab7a94afff200
Muhammad kamran <muhammad.kamran.5711@gmail.com>
2022-05-17 03:34:29 +0000

    Cors code moved from up to down

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const mongoose = require('mongoose');
 const payerrouter = require('./api/payer');
 const tablerouter = require('./api/tables');
-const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
 const port = process.env.PORT || 3333;
@@ -14,27 +14,30 @@ require('./models/tabledata')
 app.use(helmet());
 app.use(morgan("common"));
 app.use(express.json());
-app.use('/', payerrouter)
-app.use('/', tablerouter)
+
 
 app.use(cors());
+app.options('*', cors());
+// app.use(cors());
 
-app.use(cors({
-    origin: 'http://localhost:18010',
-    method: ['GET', 'POST', 'PUT', 'DELETE'],
-}))
+// app.use(cors({
+//     origin: 'http://localhost:18010',
+//     method: ['GET', 'POST', 'PUT', 'DELETE'],
+// }))
 
-app.use((req, res, next) => {
+// app.use((req, res, next) => {
     
-    res.append('Access-Control-Allow-Headers', 'Content-Type');
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:18010")
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Max-Age", "1800");
-    res.setHeader("Access-Control-Allow-Headers", "content-type");
-    res.setHeader("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS");
-    next();
-})
+//     res.append('Access-Control-Allow-Headers', 'Content-Type');
+//     res.setHeader("Access-Control-Allow-Origin", "http://localhost:18010")
+//     res.setHeader("Access-Control-Allow-Credentials", "true");
+//     res.setHeader("Access-Control-Max-Age", "1800");
+//     res.setHeader("Access-Control-Allow-Headers", "content-type");
+//     res.setHeader("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS");
+//     next();
+// })
 
+app.use('/', payerrouter)
+app.use('/', tablerouter)
 
 
 app.use('*', (req, res) => {
