7826ec4cd1911205aed114b6da3715256ea2949d
Saadkhancode <saadk6401@gmail.com>
2022-06-07 12:42:21 +0500

    done

--- src/api/user.js
+++ src/api/user.js
@@ -6,19 +6,19 @@ const router = express.Router();
 
 //Register Api
 
-router.get('/register',async(req,res)=>{
+router.get('/user',async(req,res)=>{
    const user=await userModel.find(req.data)
    res.send(user)
 })
 
-router.post("/register", async (req, res) => {
+router.post("/user", async (req, res) => {
 
-  const { fullName, email, password } = req.body;
+  const {id, firstName,lastName, email, password } = req.body;
   const userRegister = await userModel.findOne({ email });
   if (userRegister) {
       return res.send("this user is already registered")
   }
-  const newUser = new userModel({ fullName, email, password });
+  const newUser = new userModel({id, firstName,lastName, email, password });
   const savedUser = await newUser.save();
   // .catch((err) => {
   //   console.log("Error: ", err);
@@ -54,7 +54,7 @@ router.post("/register", async (req, res) => {
     process.env.JWT_SECRET
   );
 
-  res.json({ fullName:userWithEmail.fullName,email:userWithEmail.email,password:userWithEmail.password, token: jwtToken });
+  res.json({id:userWithEmail.id, firstName:userWithEmail.firstName,lastName:userWithEmail.lastName, email:userWithEmail.email,password:userWithEmail.password, token: jwtToken });
 });
 
 export default router;
