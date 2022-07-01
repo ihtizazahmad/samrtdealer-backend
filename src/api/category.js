3b8a62c3744bb5fabdcd48921538848aa5242bbf
Saadkhancode <saadk6401@gmail.com>
2022-07-01 12:14:23 +0500

    catgeory get by id api added

--- src/api/category.js
+++ src/api/category.js
@@ -6,6 +6,10 @@ router.get('/category/:_id', async(req, res) => {
     let data = await category.find(req.params).populate('product')
     res.send(data);
 })
+router.get('/category', async(req, res) => {
+    let data = await category.find(req.params).populate('product')
+    res.send(data);
+})
 
 router.post('/category', async (req, res) => {
     const { id, name, parent, extraData, categoryType, displayMangerName, order, hasPicture, active, displayManagerId, parentId, lampixIcon, translation, product, showPictures } = req.body;
