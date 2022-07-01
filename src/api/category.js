d1a99c57e90a0e93b72d148bbf2b046510d6c682
Mustafa zeb <tauheedzeb381@gmail.com.com>
2022-07-01 15:15:20 +0500

    by id only in category

--- src/api/category.js
+++ src/api/category.js
@@ -2,7 +2,7 @@ import express from 'express';
 import category from '../models/category.js'
 const router = express.Router();
 
-router.get('/category/:_id', async(req, res) => {
+router.get('/category/:id', async(req, res) => {
     let data = await category.find(req.params).populate('product')
     res.send(data);
 })
