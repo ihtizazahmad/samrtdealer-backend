25133c729c66c2bab325d171cf892333d5781c45
Saadkhancode <saadk6401@gmail.com>
2022-07-07 15:39:57 +0500

    body parser added

--- src/index.js
+++ src/index.js
@@ -37,8 +37,8 @@ const port = process.env.PORT;
 app.use(helmet());
 app.use(morgan("dev"));
 app.use(express.json());
-// app.use(bodyParser.json())
-// app.use(express.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", true);
     next();
