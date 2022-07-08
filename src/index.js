78db8e72301eadda1ffd2fa5527f8e371acd59f4
Saadkhancode <saadk6401@gmail.com>
2022-07-08 11:34:41 +0500

    cors added

--- src/index.js
+++ src/index.js
@@ -34,17 +34,20 @@ const app = express();
 dotenv.config();
 const port = process.env.PORT;
 
-app.use(cors(corsOptions));
+app.use(cors({
+    origin:'*',
+}));
 app.use(helmet());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-const corsOptions = {
-    origin:'http://localhost:18020',
-    optionSuccessStatus: 200
-}
+// const corsOptions = {
+//     origin:'http://localhost:18020',
+
+//     optionSuccessStatus: 200
+// }
 
 app.use('/api/v1', Auth, payer, tabledata, category, check, device, display, employee, language, menu, mu, order, orderitem, paymentlist, posmenuitem, posmenu,posMenuSizes, product, role, tax, translate, tables,)
 
