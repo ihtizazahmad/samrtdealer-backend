32b1c7b533a804e59beff3baba72d44651a9a34d
Saadkhancode <saadk6401@gmail.com>
2022-11-15 10:31:56 +0500

    super admin api created

--- src/api-routes/user-route.js
+++ src/api-routes/user-route.js
@@ -4,11 +4,13 @@ const routes=express.Router();
 import {getUser,
     login,
     deleteUser,
+    updateUser
 } from "../api/user.js"
 
 routes.get('/User', getUser )
 
 routes.post('/login', login )
+routes.put('/user/:_id',updateUser)
 routes.delete('/user/:email',  deleteUser)
 
 
