5149f875bd6b558b1c52c732b96a06ff9c3d065e
Saadkhancode <saadk6401@gmail.com>
2022-12-23 15:55:41 +0500

    logo product table img upload with s3 done

--- src/middlewares/uploader.js
+++ src/middlewares/uploader.js
@@ -16,20 +16,3 @@ const storage = multer.diskStorage({
   });
   
  export const upload = multer({ storage });
-// import multer from "multer";
-//  const uploaderObject = {
-//     upload: multer({
-//       storage: multer.diskStorage({
-//         destination: function (req, file, cb) {
-//           cb(null, "public");
-//         },
-//         filename: function (req, file, cb) {
-//           cb(null, file.originalname);
-//           // let temp = file.originalname.split('.');
-//           cb(null, Date.now() + "-" + file.originalname + ".jpg .png .jpeg ");
-//           // cb(null, file.fieldname + '-' + Date.now()+'.'+ temp[temp.length-1])
-//         },
-//       }),
-//     })
-//   };
-//   export default uploaderObject;
